2ab3116453c7c83ac78e4d46bcbd520167a6b913
wearedrivn <jsdcompany075@gmail.com>
2026-07-31 14:17:21 +0200

    Fix Netlify Function: use ESM export syntax

--- netlify/functions/submissions.js
+++ netlify/functions/submissions.js
@@ -10,7 +10,7 @@
 // None of these are ever sent to the browser; only this server-side function
 // can read them.
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: JSON.stringify({ error: 'Method not allowed' }) };
   }
